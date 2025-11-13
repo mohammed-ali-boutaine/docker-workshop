d23bfed68f2314415d01401c8f79d5f0367e99b9
ISTIFANO <elamiriamir7@gmail.com>
2025-11-13 23:56:10 +0100

    ajouter config airflow and streamlit and pgsql

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -1,1 +1,16 @@
-# main
+from fastapi import FastAPI
+import psycopg2
+import pandas as pd
+
+app = FastAPI()
+
+@app.get("/data")
+def get_data():
+    conn = psycopg2.connect(
+        dbname="etl_data",
+        user="airflow",
+        password="airflow",
+        host="postgres"
+    )
+    df = pd.read_sql("SELECT * FROM etl_table", conn)
+    return df.to_dict(orient="records")
